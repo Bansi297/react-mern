9ea35dc33b728135cbadca56ae0a7d0feb0617bf
Parth Dabhi <parthdabhi@mac.home>
2025-04-10 16:14:34 -0400

    responsive css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,22 +16,37 @@ const Home =() => {
     }
     return(
         <>
-            <div className="grid grid-cols-4 border-blue ">
-                {products.map((x) => {
+          
 
                         // overflow: hidden;   for show products in same line
                         // display: -webkit-box;
                         // -webkit-line-clamp: 2;
                         // -webkit-box-orient: vertical;
-                    return <div className="p-5 border shadow-inner m-1 border-2 rounded-lg border-black-900">
-                        <div onClick={() => productDetails(x.id)}>
-                            <img className="h-[150px]" src={x.img} alt="" />
-                            <h1 className="line-clamp-2">{x.title}</h1>  
-                            <h1>{x.price}</h1>
-                        </div>
-                        <button className="p-2 bg-black text-white rounded-lg w-full" onClick={() => addtocart(x.id)} >Add to cart</button>
-                    </div>
-                }) }
+               
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+            {products.map((x) => (
+                <div
+                key={x.id}
+                className="p-4 border shadow-inner border-gray-300 rounded-lg flex flex-col justify-between"
+                >
+                <div onClick={() => productDetails(x.id)} className="cursor-pointer">
+                    <img
+                    className="h-40 w-full object-contain mb-2"
+                    src={x.img}
+                    alt={x.title}
+                    />
+                    <h1 className="line-clamp-2 font-medium text-lg">{x.title}</h1>
+                    <h1 className="text-sm text-gray-700">₹{x.price}</h1>
+                </div>
+                <button
+                    className="mt-3 p-2 bg-black text-white rounded-lg w-full hover:bg-gray-800"
+                    onClick={() => addtocart(x.id)}
+                >
+                    Add to cart
+                </button>
+                </div>
+            ))}
             </div>
         </>
     )
