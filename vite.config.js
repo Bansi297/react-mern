a75c4d648c6cff7aacaea1d9d6ceb704c0181a92
Bansi297 <Bansarivora297@gmail.com>
2025-05-14 14:49:08 -0400

    Update before deploy

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
-  server: {
-    
-    
-    proxy: {
-      '/api': {
-        // target: "https://jobportal-5kw8.onrender.com",
-        target: 'http://localhost:9000',
-        changeOrigin: true,
-        secure: false
-      }
-    },
-  },
- 
+  base: '/react-mern/',
   plugins: [react()],
- base : "/react-mern" ,
+  
+  // server: {   
+//     proxy: {
+//       '/api': {
+//         // target: "https://jobportal-5kw8.onrender.com",
+//         target: 'http://localhost:9000',
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     },
+//   },
+ 
+//   plugins: [react()],
+//  base : "/react-mern/" ,
 })
