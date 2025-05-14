d0fe2e8540c752130337ff577d363d81d1c904ce
Bansi297 <Bansarivora297@gmail.com>
2025-05-14 18:05:31 -0400

    404 solved

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/react-mern">
       <App />
     </HashRouter>
   </React.StrictMode>
