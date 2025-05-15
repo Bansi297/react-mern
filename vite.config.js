0f53536ec3cbe012f0230eb5b8ab28e30dcb4f72
Bansi297 <Bansarivora297@gmail.com>
2025-05-15 13:34:16 -0400

    changes in vite

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-mern/',
-  plugins: [react()],
+
   
-  // server: {   
-//     proxy: {
-//       '/api': {
-//         // target: "https://jobportal-5kw8.onrender.com",
-//         target: 'http://localhost:9000',
-//         changeOrigin: true,
-//         secure: false
-//       }
-//     },
-//   },
+  server: {   
+    proxy: {
+      '/api': {
+        // target: "https://jobportal-5kw8.onrender.com",
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        secure: false
+      }
+    },
+  },
  
-//   plugins: [react()],
-//  base : "/react-mern/" ,
+  plugins: [react()],
+ base : "/react-mern/" ,
 })
